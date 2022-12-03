e754d6ffc5e64f73fed40f2d032d1d82aeaa9eaa
SlenPlayz <me@slen.tk>
2022-12-03 16:24:18 +0800

    Added link to server source code repo in example env file

--- lib/env.example.dart
+++ lib/env.example.dart
@@ -1,6 +1,6 @@
 // Rename this file from env.example.dart to env.dart
 
-// Enter the server URL to be used by the app (Source code to host can be found here: )
+// Enter the server URL to be used by the app (Source code to host can be found here: https://github.com/SlenPlayz/SgBusServer)
 const serverURL = '';
 
 // Get an access token at https://account.mapbox.com/access-tokens/create and add it here
