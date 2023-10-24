1799d9d4147733131f3f289ef32c857a6f6271d9
SlenPlayz <weakpole@gmail.com>
2023-10-24 17:25:22 +0800

    Update README and instructions to run

--- lib/env.example.dart
+++ lib/env.example.dart
@@ -1,6 +1,6 @@
 // Rename this file from env.example.dart to env.dart
 
-// Enter the server URL to be used by the app (Source code to host can be found here: https://github.com/SlenPlayz/SgBusServer)
+// Enter the server URL to be used by the app (Source code to host can be found here: https://github.com/SlenPlayz/SGBus-web)
 const serverURL = '';
 
 // Get an access token at https://account.mapbox.com/access-tokens/create and add it here
