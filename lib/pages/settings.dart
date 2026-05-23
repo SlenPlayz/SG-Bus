fc2f5a1e12873508cd9bba1fa70d9daa762eede3
Nelson <nelso.n@aol.com>
2026-05-23 11:51:20 +0800

    Generalising text

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -82,7 +82,7 @@ class _SettingsState extends State<Settings> {
               CustomSetting(
                 title: 'Download data',
                 leading: Icon(Icons.download),
-                subtitle: 'Re-download stop & bus data from the server',
+                subtitle: 'Re-download transit data from the server',
                 onPressed: () => Navigator.of(context).push(MaterialPageRoute(
                     builder: (context) => const DownloadPage(
                           restartOnComplete: false,
